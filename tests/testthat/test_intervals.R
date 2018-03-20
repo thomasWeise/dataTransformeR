bb2f37f778a442d02eb16d8f184804dbc7ca04f4
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-20 08:19:23 +0800

    Tests further Reduced for Travis-CI

--- tests/testthat/test_intervals.R
+++ tests/testthat/test_intervals.R
@@ -6,7 +6,7 @@ need.fast.tests <- (!(base::is.na(base::Sys.getenv("TRAVIS", unset=NA))));
 
 test_that("Test Transformation.mapIntervals (I)", {
   vec.base <- -5:5;
-  if(need.fast.tests) { vec.base <- -4:4; }
+  if(need.fast.tests) { vec.base <- -3:4; }
 
   vec.1 <- rep(vec.base, times=length(vec.base));
   vec.2 <- rep(vec.base, times=rep(length(vec.base), length(vec.base)));
@@ -104,10 +104,11 @@ test_that("Test Transformation.mapIntervals (I)", {
 
 
 test_that("Test Transformation.mapIntervals (II)", {
-  vec.1.1 <- rnorm(100);
-  vec.1.2 <- rnorm(100);
-  vec.2.1 <- rnorm(100);
-  vec.2.2 <- rnorm(100);
+  if(need.fast.tests) { n <- 50; } else { n <- 100; }
+  vec.1.1 <- rnorm(n);
+  vec.1.2 <- rnorm(n);
+  vec.2.1 <- rnorm(n);
+  vec.2.2 <- rnorm(n);
   indices <- 1:length(vec.1.1);
   trafos <- lapply(X=indices, FUN=function(i) {
     if(vec.1.1[i]==vec.1.2[i]) return(NULL);
@@ -152,10 +153,11 @@ test_that("Test Transformation.mapIntervals (II)", {
 
 
 test_that("Test Transformation.mapIntervals (III)", {
-  vec.1.1 <- runif(100);
-  vec.1.2 <- runif(100);
-  vec.2.1 <- runif(100);
-  vec.2.2 <- runif(100);
+  if(need.fast.tests) { n <- 50; } else { n <- 100; }
+  vec.1.1 <- runif(n);
+  vec.1.2 <- runif(n);
+  vec.2.1 <- runif(n);
+  vec.2.2 <- runif(n);
   indices <- 1:length(vec.1.1);
   trafos <- lapply(X=indices, FUN=function(i) {
     if(vec.1.1[i]==vec.1.2[i]) return(NULL);
@@ -350,55 +352,58 @@ test_that("Test Transformation.mapIntervals (V)", {
                       },
                       FUN.VALUE = NaN), vec.1.2);
 
-  vec.1.1 <- vec.1.1 * 0.2;
-  vec.1.2 <- vec.1.2 * 0.2;
-  vec.2.1 <- vec.2.1 * 0.2;
-  vec.2.2 <- vec.2.2 * 0.2;
-  trafos <- lapply(X=indices, FUN=function(i) {
-    if(vec.1.1[i]==vec.1.2[i]) return(NULL);
-    if(vec.2.1[i]==vec.2.2[i]) return(NULL);
-    return(Transformation.mapIntervals(vec.1.1[i], vec.1.2[i],
-                                       vec.2.1[i], vec.2.2[i]));
-  });
 
-  expect_equal(vapply(X=indices,
-                      FUN=function(i) {
-                        if(is.null(trafos[[i]])) {
-                          return(vec.2.1[i]);
-                        }
-                        return(trafos[[i]]@forward(vec.1.1[i]));
-                      },
-                      FUN.VALUE = NaN), vec.2.1);
-  expect_equal(vapply(X=indices,
-                      FUN=function(i) {
-                        if(is.null(trafos[[i]])) {
-                          return(vec.2.2[i]);
-                        }
-                        return(trafos[[i]]@forward(vec.1.2[i]));
-                      },
-                      FUN.VALUE = NaN), vec.2.2);
-  expect_equal(vapply(X=indices,
-                      FUN=function(i) {
-                        if(is.null(trafos[[i]])) {
-                          return(vec.1.1[i]);
-                        }
-                        return(trafos[[i]]@backward(vec.2.1[i]));
-                      },
-                      FUN.VALUE = NaN), vec.1.1);
-  expect_equal(vapply(X=indices,
-                      FUN=function(i) {
-                        if(is.null(trafos[[i]])) {
-                          return(vec.1.2[i]);
-                        }
-                        return(trafos[[i]]@backward(vec.2.2[i]));
-                      },
-                      FUN.VALUE = NaN), vec.1.2);
+  if(!need.fast.tests) {
+    vec.1.1 <- vec.1.1 * 0.2;
+    vec.1.2 <- vec.1.2 * 0.2;
+    vec.2.1 <- vec.2.1 * 0.2;
+    vec.2.2 <- vec.2.2 * 0.2;
+    trafos <- lapply(X=indices, FUN=function(i) {
+      if(vec.1.1[i]==vec.1.2[i]) return(NULL);
+      if(vec.2.1[i]==vec.2.2[i]) return(NULL);
+      return(Transformation.mapIntervals(vec.1.1[i], vec.1.2[i],
+                                         vec.2.1[i], vec.2.2[i]));
+    });
+
+    expect_equal(vapply(X=indices,
+                        FUN=function(i) {
+                          if(is.null(trafos[[i]])) {
+                            return(vec.2.1[i]);
+                          }
+                          return(trafos[[i]]@forward(vec.1.1[i]));
+                        },
+                        FUN.VALUE = NaN), vec.2.1);
+    expect_equal(vapply(X=indices,
+                        FUN=function(i) {
+                          if(is.null(trafos[[i]])) {
+                            return(vec.2.2[i]);
+                          }
+                          return(trafos[[i]]@forward(vec.1.2[i]));
+                        },
+                        FUN.VALUE = NaN), vec.2.2);
+    expect_equal(vapply(X=indices,
+                        FUN=function(i) {
+                          if(is.null(trafos[[i]])) {
+                            return(vec.1.1[i]);
+                          }
+                          return(trafos[[i]]@backward(vec.2.1[i]));
+                        },
+                        FUN.VALUE = NaN), vec.1.1);
+    expect_equal(vapply(X=indices,
+                        FUN=function(i) {
+                          if(is.null(trafos[[i]])) {
+                            return(vec.1.2[i]);
+                          }
+                          return(trafos[[i]]@backward(vec.2.2[i]));
+                        },
+                        FUN.VALUE = NaN), vec.1.2);
+  }
 })
 
 
 test_that("Test Transformation.normalizeInterval (I)", {
   vec.base <- -5:5
-  if(need.fast.tests) { vec.base <- -4:4; }
+  if(need.fast.tests) { vec.base <- -4:3; }
 
   vec.1 <- rep(vec.base, times=length(vec.base));
 
@@ -451,10 +456,11 @@ test_that("Test Transformation.normalizeInterval (I)", {
 
 
 test_that("Test Transformation.normalizeInterval (II)", {
-  vec.1.1 <- rnorm(100);
-  vec.1.2 <- rnorm(100);
-  vec.2.1 <- rep(0, 100);
-  vec.2.2 <- rep(1, 100);
+  if(need.fast.tests) { n <- 50; } else { n <- 100; }
+  vec.1.1 <- rnorm(n);
+  vec.1.2 <- rnorm(n);
+  vec.2.1 <- rep(0, n);
+  vec.2.2 <- rep(1, n);
   indices <- 1:length(vec.1.1);
   trafos <- lapply(X=indices, FUN=function(i) {
     if(vec.1.1[i]==vec.1.2[i]) return(NULL);
@@ -499,10 +505,11 @@ test_that("Test Transformation.normalizeInterval (II)", {
 
 
 test_that("Test Transformation.normalizeInterval (III)", {
-  vec.1.1 <- runif(100);
-  vec.1.2 <- runif(100);
-  vec.2.1 <- rep(0, 100);
-  vec.2.2 <- rep(1, 100);
+  if(need.fast.tests) { n <- 50; } else { n <- 100; }
+  vec.1.1 <- runif(n);
+  vec.1.2 <- runif(n);
+  vec.2.1 <- rep(0, n);
+  vec.2.2 <- rep(1, n);
   indices <- 1:length(vec.1.1);
   trafos <- lapply(X=indices, FUN=function(i) {
     if(vec.1.1[i]==vec.1.2[i]) return(NULL);
